e6b0e1db2d4e1cf81ae1042df097d089cda94755
Sebastian-CR7 <2025ds076@utez.edu.mx>
2026-01-14 09:03:07 -0600

    Se guardo exitosamente

---
+++ BuenasPracticas/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner
+public class Main {
+    public static double IVA=0.16;
+    public static double UMBRAL_DESCUENTO=1000;
+    public static double DESCUENTO=0.10;
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        double
+
+    }
+        }
