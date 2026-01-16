de36985b6f1cf5ea3cd26c0cf135ff558476de05
Sebastian-CR7 <20253ds076@utez.edu.mx>
2026-01-15 20:04:46 -0600

    feat: agregar operaciones

--- MergeWithOutConflict/src/Main.java
+++ MergeWithOutConflict/src/Main.java
@@ -1,10 +1,8 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Semana 2");
         System.out.println("Version Base");
+        System.out.println("2 + 2 = 4");
+        System.out.println("5 * 3 = 15");
     }
 }
