7e288bdd88ee07f8c1d7e4db65a2b16b745a2618
Sebastian-CR7 <20253ds076@utez.edu.mx>
2026-01-16 22:45:44 -0600

    Se guardo el codigo corregido

--- ActividadBuenasPracticas/src/Main.java
+++ ActividadBuenasPracticas/src/Main.java
@@ -1,9 +1,33 @@
 import java.util.Scanner;
-public class Main{
-    public static void main(String[]a){
-        Scanner sc=new Scanner(System.in);
-        System.out.print("n:");int n=sc.nextInt();
-        int s=0;
-        for(int i=1;i<=n;i++){s+=i;}
-        System.out.println("r:"+s);
-    }}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numLimite = pedirEntero(sc, "Ingresa un numero entero (positivo): ");
+        int sumTotal = sumarHastaN(numLimite);
+        System.out.println("El resultado: "+sumTotal);
+    }
+
+    /**
+     * Metodo para obtener un entero con un mensaje personalizado
+     * @param-> Se le pide al usuario que ingrese un numero
+     * @return-> Numero entero
+     */
+    public static int pedirEntero (Scanner sc, String s){
+        System.out.println(s);
+        return sc.nextInt();
+    }
+
+    /**
+     * Calcula la suma acumulada
+     * @param-> Limite es el numero maximo a sumar
+     * @return->La suma total
+     */
+    public static int sumarHastaN (int limite){
+        int acumularSum = 0;
+        for (int i = 1; i <= limite; i++) {
+            acumularSum += i;
+        }
+        return acumularSum;
+    }
+}
